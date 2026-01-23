64edd2de17ed06cf95a2631cfe82e74a57434db5
Eder <ederlemesmt@gmail.com>
2026-01-23 00:30:45 -0400

    Mudança no titulo da pagina principal

--- src/app/pages/HomePage.tsx
+++ src/app/pages/HomePage.tsx
@@ -4,7 +4,7 @@ export default function HomePage() {
     return (
         <div style={{ display: 'grid', gap: 12 }}>
             <h1 className="text-3xl font-bold underline text-blue-600">
-                Hello World!
+                Sistema de gestão de Pets e Tutores
             </h1>
             <nav style={{ display: 'flex', gap: 8 }}>
                 <Link to="/pets">Ver Pets</Link>
