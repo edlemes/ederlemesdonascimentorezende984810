1ad4874a2d50513d72da4798284736dda0eb3e8d
Eder <ederlemesmt@gmail.com>
2026-02-05 01:37:32 -0300

    feat(pets): adicionar rota para vincular tutor

--- src/app/features/pets/pets.routes.tsx
+++ src/app/features/pets/pets.routes.tsx
@@ -2,14 +2,16 @@ import { Route, Routes } from "react-router-dom"
 import { PetsListPage } from "./pages/PetsListPage"
 import { PetDetailPage } from "./pages/PetDetailPage"
 import { PetFormPage } from "./pages/PetFormPage"
+import { LinkTutorPage } from "./pages/LinkTutorPage"
 
 export function PetsRoutes() {
   return (
     <Routes>
       <Route index element={<PetsListPage />} />
-      <Route path=":id" element={<PetDetailPage />} />
       <Route path="novo" element={<PetFormPage />} />
+      <Route path=":id" element={<PetDetailPage />} />
       <Route path=":id/editar" element={<PetFormPage />} />
+      <Route path=":id/vincular-tutor" element={<LinkTutorPage />} />
     </Routes>
   )
 }
